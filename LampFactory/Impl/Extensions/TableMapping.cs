e1a0879582edddf7e0c39d50ed1358a40ecd034a
csombok <csombok@gmail.com>
2016-09-19 17:14:26 +0200

    Hozzáadtam egy kezdetleges model-t ami a fejemben összeállt eddig. Ezt a tesztadat generáláshoz és a validáláshoz talán tudjuk használni. ha mégsem kidobjuk :)

---
+++ LampFactory/Impl/Extensions/TableMapping.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Impl.Extensions
+{
+    public static class TableMapping
+    {
+        public const byte Wall0 = 0;
+        public const byte Wall1 = 1;
+        public const byte Wall2 = 2;
+        public const byte Wall3 = 3;
+        public const byte Wall4 = 4;
+        public const byte Wall = 5;
+        public const byte Free = 6;
+        public const byte Lamp = 7;
+
+        private static readonly Dictionary<char, byte> Mapping = new Dictionary<char, byte>()
+        {
+            {'0',Wall0 },
+            {'1',Wall1 },
+            {'2',Wall2 },
+            {'3',Wall3 },
+            {'4',Wall4 },
+            {'#',Wall },
+            {' ',Free },
+            {'o',Lamp },
+        };
+
+        public static char Map(this byte value)
+        {
+            return Mapping.Single(kv => kv.Value.Equals(value)).Key;
+        }
+
+        public static bool IsWall(this byte value)
+        {
+            var intValue = (int)value;
+            return intValue >= 0 && intValue <= 5;
+        }
+
+        public static byte Map(this char key)
+        {
+            return Mapping[key];
+        }
+    }
+}
---
+++ LampFactory/Impl/Model/Table.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Impl.Extensions;
+
+namespace Impl.Model
+{
+    public class Table
+    {
+        private readonly int _length;
+        private readonly byte[][] _table;
+
+        public Table(string rawTable)
+        {
+            var lines = rawTable.Split();
+            _length = lines.Length;
+            _table = new byte[_length][];
+
+            // TODO: Try to use multiple tasks to setup the initial model
+            for (var row = 0; row < _length; row++)
+            {
+                for (var column = 0; column < _length; column++)
+                {
+                    _table[row][column] = lines[row][column].Map();
+                }
+            }
+        }
+
+        public bool IsCorrect()
+        {
+            // TODO: Try to use multiple tasks to evaluate correctness
+            for (var row = 0; row < _length; row++)
+            {
+                for (var column = 0; column < _length; column++)
+                {
+                    if (!IsCorrect(row, column))
+                        return false;
+                }
+            }
+
+            return true;
+        }
+
+        public void SetupLamp(int row, int column)
+        {
+            if (_table[row][column] != TableMapping.Free)
+            {
+                throw new InvalidOperationException("Setting up a lamp is not allowed since the cell is already used.");
+            }
+
+            _table[row][column] = TableMapping.Lamp;
+        }
+
+        private bool IsCovered(int row, int column)
+        {
+            var value = _table[row][column];
+
+            if (value != TableMapping.Free && value != TableMapping.Lamp) return false;
+
+            Func<int, int, bool> evaluateLamps = (x, y) =>
+            {
+                var rowIndex = row + x;
+                var colIndex = column + y;
+
+                while (rowIndex >= 0 && rowIndex < _length && colIndex >= 0 && colIndex < _length)
+                {
+                    var next = _table[rowIndex][colIndex];
+
+                    if (next == TableMapping.Lamp)
+                        return true;
+
+                    if (next.IsWall())
+                        break;
+
+                    rowIndex = rowIndex + x;
+                    colIndex = colIndex + y;
+                }
+
+                return false;
+            };
+
+            var taskHorizontalLeft = new Task<bool>(() => evaluateLamps(-1, 0));
+            var taskHorizontalRight = new Task<bool>(() => evaluateLamps(1, 0));
+            var taskVerticalUp = new Task<bool>(() => evaluateLamps(0, 1));
+            var taskVerticalDown = new Task<bool>(() => evaluateLamps(0, -1));
+
+            return Task.WhenAll(taskHorizontalLeft, taskHorizontalRight, taskVerticalUp, taskVerticalDown)
+                .Result
+                .Any(x => x);
+        }
+
+        private bool IsCorrect(int row, int column)
+        {
+            var value = _table[row][column];
+
+            switch (value)
+            {
+                case TableMapping.Lamp:
+                    return !IsCovered(row, column);
+                case TableMapping.Free:
+                    return IsCovered(row, column);
+                case TableMapping.Wall:
+                    return true;
+                case TableMapping.Wall0:
+                case TableMapping.Wall1:
+                case TableMapping.Wall2:
+                case TableMapping.Wall3:
+                case TableMapping.Wall4:
+                    return NeighborLamps(row, column) == value;
+                default:
+                    return false;
+            }
+        }
+
+        private int NeighborLamps(int row, int column)
+        {
+            Func<int, int, int> evaluateNeighbor = (x, y) =>
+            {
+                var rowIndex = row + x;
+
+                if (rowIndex < 0) return 0;
+                if (rowIndex >= _length) return 0;
+
+                var colIndex = column + y;
+
+                if (colIndex < 0) return 0;
+                if (colIndex >= _length) return 0;
+
+                return _table[rowIndex][colIndex] == TableMapping.Lamp ? 1 : 0;
+            };
+
+            var taskHorizontalLeft = new Task<int>(() => evaluateNeighbor(-1, 0));
+            var taskHorizontalRight = new Task<int>(() => evaluateNeighbor(1, 0));
+            var taskVerticalUp = new Task<int>(() => evaluateNeighbor(0, 1));
+            var taskVerticalDown = new Task<int>(() => evaluateNeighbor(0, -1));
+
+            return Task.WhenAll(taskHorizontalLeft, taskHorizontalRight, taskVerticalUp, taskVerticalDown)
+                .Result
+                .Sum(x => x);
+        }
+    }
+}
