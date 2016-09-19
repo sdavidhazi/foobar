c23332faffca845252dc5e4b06bfa709f7ef19f3
Sandor Davidhazi <sdavidhazi@slb.com>
2016-09-19 16:32:12 +0200

    Solution skeleton

---
+++ LampFactory/Impl.Test/SolverTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Shouldly;
+
+namespace Impl.Test
+{
+    [TestFixture]
+    public class SolverTest
+    {
+        [Test]
+        public void Solve_Is_Not_Implemented()
+        {
+            // Arrange
+            var solver = new Solver();
+
+            // Act
+            // Assert
+            Should.Throw<NotImplementedException>(() => solver.Solve(""));
+        }
+    }
+}
---
+++ LampFactory/Impl/Solver.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Contracts;
+
+namespace Impl
+{
+    public class Solver : ISolver
+    {
+        public string Solve(string table)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
