0a14adc51c5e6207c801d5362c914bbb56aa47e9
csombok <csombok@gmail.com>
2016-09-22 17:15:02 +0200

    Added stack and parallel solver implementations of ISolver upon the same algorythm logic - though it doesn't increase the performance as a first look

--- LampFactory/Impl/ParallelSolver.cs
+++ LampFactory/Impl/ParallelSolver.cs
@@ -1,6 +1,7 @@
 ﻿using Contracts;
 using Impl.Model;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,46 +9,52 @@ namespace Impl
 {
     public class ParallelSolver : ISolver
     {
-        private Table Solve2(Table table)
+        private Table SolveTable(Table table)
         {
-            if (table.IsReady())
-                return table;
-            var list = table.GetMinCellList();
-            if (list == null || list.Count == 0)
-                return null;
-            if (list.Count == 1)
-            {
-                table.SetupLamp(list[0].Item1, list[0].Item2);
-                if (table.Invalid)
-                    return null;
-                return Solve2(table);
-            }
+            table.SetupLightBesideWalls();
+
+            var tableSnapshots = new ConcurrentStack<Table>();
+            tableSnapshots.Push(table.Clone());
 
             ConcurrentBag<Table> results = new ConcurrentBag<Table>();
-            Parallel.ForEach(list, (cell, loopState) =>
-               {
-                   var clone = table.Clone();
-                   clone.SetupLamp(cell.Item1, cell.Item2);
 
-                   if (!clone.Invalid)
-                   {
-                       var result = Solve2(clone);
-                       if (result != null)
-                       {
-                           results.Add(result);
-                           loopState.Stop();
-                       }
-                   }
-               });
+            while (tableSnapshots.Count > 0)
+            {
+                Table snapshot;
+                if (!tableSnapshots.TryPop(out snapshot)) break;
 
-            return results.FirstOrDefault();
+                var list = snapshot.GetMinCellList();
+
+                Parallel.ForEach(list, (cell, loopState) =>
+                {
+                    var clone = snapshot.Clone();
+                    clone.SetupLamp(cell.Item1, cell.Item2);
+
+                    if (!clone.Invalid)
+                    {
+                        if (clone.IsReady())
+                        {
+                            results.Add(clone);
+                            loopState.Stop();
+                        }
+                        else
+                        {
+                            tableSnapshots.Push(clone);
+                        }
+                    }
+                });
+
+                if (results.Any())
+                    return results.FirstOrDefault();
+            }
+
+            return null;
         }
 
         public string Solve(string table)
         {
             var puzzle = new Table(table);
-            puzzle.SetupLightBesideWalls();
-            Table result = Solve2(puzzle);
+            Table result = SolveTable(puzzle);
             return result?.ToString().Replace('x', ' ');
         }
     }
---
+++ LampFactory/Impl/StackSolver.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Contracts;
+using Impl.Model;
+
+namespace Impl
+{
+    public class StackSolver : ISolver
+    {
+        private Table SolveTable(Table table)
+        {
+            table.SetupLightBesideWalls();
+
+            var tableSnapshots = new Stack<Table>();
+            tableSnapshots.Push(table.Clone());
+
+            while (tableSnapshots.Count > 0)
+            {
+                var snapshot = tableSnapshots.Pop();
+                var list = snapshot.GetMinCellList();
+
+                foreach (var cell in list)
+                {
+                    var clone = snapshot.Clone();
+                    clone.SetupLamp(cell.Item1, cell.Item2);
+
+                    if (clone.Invalid)
+                        continue;
+                    
+                    if (clone.IsReady())
+                        return clone;
+
+                    tableSnapshots.Push(clone);
+                }
+            }
+
+            return null;
+        }
+
+        public string Solve(string table)
+        {
+            var puzzle = new Table(table);            
+            Table result = SolveTable(puzzle);
+            return result?.ToString().Replace('x', ' ');
+        }
+    }
+}
